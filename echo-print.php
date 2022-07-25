cee4c6a855fcf69ffbb54f4d86f22bbc2644ab9f
baptuDenjer <niklas.imbiri@yahoo.com>
2022-07-25 11:01:15 +0900

    editting some file and adding new files

---
+++ echo-print.php
@@ -0,0 +1,10 @@
+<?php
+	// Using "echo" to display data
+	echo "<h1>Papua Tech Community</h1>";
+	echo "<p>Sharing and learning together in the community</p>";
+	
+	// Using "print" to display data
+	
+	print "<h1>Papua Coding School</h1>";
+	print "<p>Learn how to code from the basic through our courses.</p>";
+?>
---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+	//Variable Declaring in PHP
+	
+	$title = "Highland Roastery";
+	$tag_line = "Papua Coding School";
+	$endl = "<br>";
+	
+	
+	//Printing out the value of the variables
+	echo $title, "<hr>";
+	echo "Komunitas " , $title, "<hr>";
+	
+	echo $title, " Community";
+?>
